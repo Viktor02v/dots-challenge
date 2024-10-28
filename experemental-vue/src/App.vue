bfa299195c7ef1c3c456b2f2bfad5878eb3acbd5
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-10-28 05:18:20 +0100

    first commit

---
+++ experemental-vue/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref, provide } from "vue";
+import Child from "./components/Child.vue";
+
+// const count = ref(0);
+
+// provide("count", count);
+</script>
+
+<template>
+  <Child />
+</template>
---
+++ experemental-vue/src/components/Child.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+
+const capitalize = ref("");
+
+const updateValue = (event) => {
+  // Capitalize the first letter and make the rest lowercase
+  const value = event.target.value;
+  capitalize.value =
+    value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+};
+</script>
+
+<template>
+  <input
+    type="text"
+    class="border border-gray-300"
+    :value="capitalize"
+    @input="updateValue"
+  />
+  <p>Input: {{ capitalize }}</p>
+</template>
